8f2dbc7a64ffe840cc308f8a123052cc1d4800c6
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-01-28 15:52:08 -0500

    added html and queryselectors in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 ///The Weather Man Forecast Website///
+
+let weatherForm = document.querySelector('#weather-form');
+let cityButtons = document.querySelector('#city-buttons');
+let cityInput = document.querySelector('#example');
+let showResults = document.querySelector('#show-results');
+let searchTerm = document.querySelector('#show-search-term');
